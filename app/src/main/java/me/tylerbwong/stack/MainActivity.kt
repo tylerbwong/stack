ca1b637042b6d85ed821fd1b57f8fd676b87fd56
brittanyberlanga <brittany.berlanga@gmail.com>
2018-03-18 18:07:56 -0700

    Derp #1

---
+++ app/src/main/java/me/tylerbwong/stack/MainActivity.kt
@@ -0,0 +1,12 @@
+package me.tylerbwong.stack
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
