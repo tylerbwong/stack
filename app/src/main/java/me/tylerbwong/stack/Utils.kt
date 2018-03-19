45e36f45e75d674b6feb8d2113a1b9f1ed8c892f
Tyler Wong <tbwong3@gmail.com>
2018-03-19 00:27:41 -0700

    Add toHtml() extension function for formatting questions

--- app/src/main/java/me/tylerbwong/stack/Utils.kt
+++ app/src/main/java/me/tylerbwong/stack/Utils.kt
@@ -1,6 +1,9 @@
 package me.tylerbwong.stack
 
+import android.os.Build
 import android.support.annotation.LayoutRes
+import android.text.Html
+import android.text.Spanned
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,3 +12,9 @@ fun ViewGroup.inflateWithoutAttaching(@LayoutRes resId: Int): View? =
         LayoutInflater.from(context).inflate(resId, this, false)
 
 fun NOOP(message: String = "No operation needed."): Nothing = TODO(message)
+
+fun String.toHtml(): Spanned = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+    Html.fromHtml(this, Html.FROM_HTML_MODE_LEGACY)
+} else {
+    Html.fromHtml(this)
+}
--- app/src/main/java/me/tylerbwong/stack/presentation/questions/QuestionHolder.kt
+++ app/src/main/java/me/tylerbwong/stack/presentation/questions/QuestionHolder.kt
@@ -6,12 +6,15 @@ import android.view.View
 import android.widget.TextView
 import me.tylerbwong.stack.R
 import me.tylerbwong.stack.data.model.Question
+import me.tylerbwong.stack.toHtml
 
 class QuestionHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     private val title: TextView = ViewCompat.requireViewById(itemView, R.id.title)
+    private val link: TextView = ViewCompat.requireViewById(itemView, R.id.link)
 
     fun bind(question: Question) {
-        title.text = question.title
+        title.text = question.title.toHtml()
+        link.text = question.link
     }
 }
