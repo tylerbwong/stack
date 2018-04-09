d091c9207206d0969324051cc7077dfab4a63977
Tyler Wong <tbwong3@gmail.com>
2018-04-08 21:58:40 -0700

    Fix crashes

--- app/src/main/java/me/tylerbwong/stack/presentation/StackApplication.kt
+++ app/src/main/java/me/tylerbwong/stack/presentation/StackApplication.kt
@@ -1,14 +1,18 @@
 package me.tylerbwong.stack.presentation
 
 import android.app.Application
+import io.reactivex.plugins.RxJavaPlugins
 import me.tylerbwong.stack.BuildConfig
 import timber.log.Timber
 
 class StackApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        RxJavaPlugins.setErrorHandler(Timber::e)
+
         if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree());
+            Timber.plant(Timber.DebugTree())
         }
     }
 }
