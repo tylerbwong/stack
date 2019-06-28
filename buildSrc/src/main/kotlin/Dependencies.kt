046ba8ad356753b305c73a6dffd8f77e45d9668f
Tyler Wong <tbwong3@gmail.com>
2019-06-27 19:26:51 -0700

    Update markwon

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -12,7 +12,7 @@ object Versions {
     const val kotlinxCoroutines = "1.2.2"
     const val ktxActivityExtensions = "1.0.0-beta01"
     const val ktxLifecycleExtensions = "2.2.0-alpha01"
-    const val markwon = "3.0.1"
+    const val markwon = "3.0.2"
     const val materialComponents = "1.1.0-alpha07"
     const val okHttp = "4.0.0"
     const val retrofit = "2.6.0"
