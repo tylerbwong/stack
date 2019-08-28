9e01938f5d7bc829593109676077e0565003e8eb
Tyler Wong <tbwong3@gmail.com>
2019-08-27 23:25:29 -0700

    Update Markwon to 4.1.0 (#31)

--- app/src/main/java/me/tylerbwong/stack/ui/utils/markdown/CustomTabsHelper.kt
+++ app/src/main/java/me/tylerbwong/stack/ui/utils/markdown/CustomTabsHelper.kt
@@ -8,9 +8,10 @@ import android.text.TextUtils
 import android.view.View
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.browser.customtabs.CustomTabsService
+import io.noties.markwon.LinkResolver
+import io.noties.markwon.core.spans.LinkSpan
 import me.tylerbwong.stack.R
 import me.tylerbwong.stack.ui.theme.ThemeManager
-import ru.noties.markwon.core.spans.LinkSpan
 import timber.log.Timber
 
 private const val STABLE_PACKAGE = "com.android.chrome"
@@ -20,7 +21,7 @@ private const val LOCAL_PACKAGE = "com.google.android.apps.chrome"
 
 private var packageName: String? = null
 
-class CustomTabsLinkResolver : LinkSpan.Resolver {
+class CustomTabsLinkResolver : LinkResolver {
     override fun resolve(view: View, link: String) = launchCustomTab(view.context, link)
 }
 
