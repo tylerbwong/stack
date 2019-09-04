ea7c90d20ae1d976ea9a42adff7dbb08605cc4c8
Tyler Wong <tbwong3@gmail.com>
2019-09-03 21:32:42 -0700

    Fix Profile Page (#46)

--- app/src/main/java/me/tylerbwong/stack/data/network/ServiceProvider.kt
+++ app/src/main/java/me/tylerbwong/stack/data/network/ServiceProvider.kt
@@ -2,7 +2,9 @@ package me.tylerbwong.stack.data.network
 
 import me.tylerbwong.stack.BuildConfig
 import me.tylerbwong.stack.data.auth.AuthInterceptor
-import me.tylerbwong.stack.data.network.service.StackService
+import me.tylerbwong.stack.data.network.service.AuthService
+import me.tylerbwong.stack.data.network.service.QuestionService
+import me.tylerbwong.stack.data.network.service.UserService
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -10,7 +12,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object ServiceProvider {
 
-    val okHttpClient by lazy {
+    private val okHttpClient by lazy {
         val okHttpClientBuilder = OkHttpClient.Builder()
                 .addInterceptor(AuthInterceptor())
 
@@ -23,13 +25,24 @@ object ServiceProvider {
         okHttpClientBuilder.build()
     }
 
-    val stackService: StackService by lazy {
+    private val retrofit by lazy {
         Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .client(okHttpClient)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
-                .create(StackService::class.java)
+    }
+
+    val authService: AuthService by lazy {
+        retrofit.create(AuthService::class.java)
+    }
+
+    val userService: UserService by lazy {
+        retrofit.create(UserService::class.java)
+    }
+
+    val questionService: QuestionService by lazy {
+        retrofit.create(QuestionService::class.java)
     }
 
     private const val BASE_URL = "https://api.stackexchange.com/2.2/"
---
+++ app/src/main/java/me/tylerbwong/stack/data/network/service/StackConstants.kt
@@ -0,0 +1,25 @@
+package me.tylerbwong.stack.data.network.service
+
+import me.tylerbwong.stack.data.model.ACTIVITY
+import me.tylerbwong.stack.data.model.DESC
+import me.tylerbwong.stack.data.model.Order
+import me.tylerbwong.stack.data.model.Sort
+
+// query params
+internal const val ACCESS_TOKEN = "accessToken"
+internal const val SITE_PARAM = "site"
+internal const val PAGE_SIZE_PARAM = "pagesize"
+internal const val PAGE_PARAM = "page"
+internal const val FILTER_PARAM = "filter"
+internal const val TAGGED_PARAM = "tagged"
+internal const val KEY_PARAM = "key"
+internal const val SEARCH_PARAM = "q"
+
+// defaults
+internal const val DEFAULT_SITE = "stackoverflow"
+@Sort
+internal const val DEFAULT_SORT = ACTIVITY
+@Order
+internal const val DEFAULT_ORDER = DESC
+internal const val DEFAULT_PAGE_SIZE = 50
+internal const val DEFAULT_PAGE = 1
