22ee00b7b920cc24ad39f5a5d0af1a6a95d73b21
Tyler Wong <tbwong3@gmail.com>
2019-09-13 02:13:03 -0700

    Update QuestionDetailActionDataModel.kt

--- app/src/main/java/me/tylerbwong/stack/ui/questions/detail/QuestionDetailActionDataModel.kt
+++ app/src/main/java/me/tylerbwong/stack/ui/questions/detail/QuestionDetailActionDataModel.kt
@@ -4,8 +4,8 @@ import me.tylerbwong.stack.data.model.Question
 import me.tylerbwong.stack.ui.utils.DynamicDataModel
 
 class QuestionDetailActionDataModel(
-        handler: QuestionDetailActionHandler,
-        question: Question
+    handler: QuestionDetailActionHandler,
+    question: Question
 ) : DynamicDataModel(), QuestionDetailActionHandler by handler {
 
     internal val downVoteCount = question.downVoteCount
@@ -18,11 +18,11 @@ class QuestionDetailActionDataModel(
     override fun areItemsThemSame(other: DynamicDataModel) = other is QuestionDetailActionDataModel
 
     override fun areContentsTheSame(
-            other: DynamicDataModel
+        other: DynamicDataModel
     ) = other is QuestionDetailActionDataModel && other.downVoteCount == downVoteCount &&
-            other.downvoted == downvoted && other.favoriteCount == favoriteCount &&
-            other.favorited == favorited && other.upVoteCount == upVoteCount &&
-            other.upvoted == upvoted
+        other.downvoted == downvoted && other.favoriteCount == favoriteCount &&
+        other.favorited == favorited && other.upVoteCount == upVoteCount &&
+        other.upvoted == upvoted
 
     override fun getViewCreator() = ::QuestionDetailActionHolder
 }
