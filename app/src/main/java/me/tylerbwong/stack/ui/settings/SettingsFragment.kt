0cf0876a498632feb87f05a04bb372fff695b7c7
Victor <victor.a.vicari@gmail.com>
2019-09-23 09:42:33 -0300

    remove duplicate dependency at build.gradle.kts. Removed unused code at settingsFragment.kt and removed all stars imports at mainActivity.kt

--- app/src/main/java/me/tylerbwong/stack/ui/settings/SettingsFragment.kt
+++ app/src/main/java/me/tylerbwong/stack/ui/settings/SettingsFragment.kt
@@ -8,12 +8,6 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings_preference, rootKey)
-        /*Preference(context).apply {
-            key = "simple_preference"
-            title = "version"
-            summary = "some usual summary"
-        }*/
-
     }
 
 }
