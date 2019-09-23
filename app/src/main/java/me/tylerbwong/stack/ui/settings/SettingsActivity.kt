219f12230bdb36a0814e892c58d4fd704e002fd2
Victor <victor.a.vicari@gmail.com>
2019-09-23 10:54:59 -0300

    conflicts solved after rebase

--- app/src/main/java/me/tylerbwong/stack/ui/settings/SettingsActivity.kt
+++ app/src/main/java/me/tylerbwong/stack/ui/settings/SettingsActivity.kt
@@ -12,12 +12,15 @@ class SettingsActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-
         setSupportActionBar(settings_toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.setTitle(R.string.settings)
 
-        text_app_version.text = resources.getString(R.string.version_name)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = resources.getString(R.string.settings)
+
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.frameContainer, SettingsFragment())
+            .commit()
     }
 
     companion object {
