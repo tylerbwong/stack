7a8fdbb2e8b45d832de687bde9afb364fad2ae64
Tyler Wong <tbwong3@gmail.com>
2019-09-24 01:28:09 -0700

    Fix comments auth bug

--- app/src/main/java/me/tylerbwong/stack/ui/comments/CommentsViewModel.kt
+++ app/src/main/java/me/tylerbwong/stack/ui/comments/CommentsViewModel.kt
@@ -3,6 +3,7 @@ package me.tylerbwong.stack.ui.comments
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import me.tylerbwong.stack.R
+import me.tylerbwong.stack.data.auth.AuthStore
 import me.tylerbwong.stack.data.network.ServiceProvider
 import me.tylerbwong.stack.data.network.service.CommentService
 import me.tylerbwong.stack.ui.BaseViewModel
@@ -10,18 +11,28 @@ import me.tylerbwong.stack.ui.utils.DynamicDataModel
 import me.tylerbwong.stack.ui.utils.EmptyDataModel
 
 class CommentsViewModel(
-    private val service: CommentService = ServiceProvider.commentService
+    private val service: CommentService = ServiceProvider.commentService,
+    private val authStore: AuthStore = AuthStore
 ) : BaseViewModel() {
 
     internal val data: LiveData<List<DynamicDataModel>>
         get() = _data
     private val _data = MutableLiveData<List<DynamicDataModel>>()
 
+    private val isAuthenticated: Boolean
+        get() = authStore.isAuthenticatedLiveData.value ?: false
+
     internal var postId = -1
 
     fun fetchComments() {
         launchRequest {
-            _data.value = service.getPostComments(postId).items
+            val commentsResponse = if (isAuthenticated) {
+                service.getPostCommentsAuth(postId)
+            } else {
+                service.getPostComments(postId)
+            }
+
+            _data.value = commentsResponse.items
                 .map { CommentDataModel(it) }
                 .ifEmpty { listOf(EmptyDataModel(R.string.no_comments)) }
         }
