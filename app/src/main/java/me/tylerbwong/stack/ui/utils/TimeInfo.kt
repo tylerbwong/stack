8104ee1950d969aa6e22fa3e62327871fd1a4d1e
Victor Vicari <victor.a.vicari@gmail.com>
2019-09-26 16:20:49 -0300

    timeInfo utility class

--- app/src/main/java/me/tylerbwong/stack/ui/utils/TimeInfo.kt
+++ app/src/main/java/me/tylerbwong/stack/ui/utils/TimeInfo.kt
@@ -18,14 +18,14 @@ fun type(view: View, type: Type): String =
         Type.EDITED -> view.resources.getQuantityString(R.plurals.type, 2)
     }
 
-fun time(view: View, creation_date: Long): String {
+fun timeCounter(view: View, creation_date: Long): String {
     val creationDate = DateTime.fromUnix(creation_date * 1000)
     val dateCalculation = (creationDate until DateTime.now()).span
 
     return when {
         dateCalculation.years > 1 -> view.resources.getQuantityString(
             R.plurals.year,
-            0,
+            1,
             dateCalculation.years
         )
         dateCalculation.years == 1 -> view.resources.getQuantityString(
@@ -33,7 +33,7 @@ fun time(view: View, creation_date: Long): String {
             0,
             dateCalculation.years
         )
-        dateCalculation.years < 1 -> view.resources.getQuantityString(
+        dateCalculation.months > 1 -> view.resources.getQuantityString(
             R.plurals.month,
             1,
             dateCalculation.months
@@ -43,7 +43,7 @@ fun time(view: View, creation_date: Long): String {
             0,
             dateCalculation.months
         )
-        dateCalculation.months < 1 -> view.resources.getQuantityString(
+        dateCalculation.weeks > 1 -> view.resources.getQuantityString(
             R.plurals.week,
             1,
             dateCalculation.weeks
@@ -53,7 +53,7 @@ fun time(view: View, creation_date: Long): String {
             0,
             dateCalculation.weeks
         )
-        dateCalculation.weeks < 1 -> view.resources.getQuantityString(
+        dateCalculation.days > 1 -> view.resources.getQuantityString(
             R.plurals.day,
             1,
             dateCalculation.days
@@ -63,7 +63,7 @@ fun time(view: View, creation_date: Long): String {
             0,
             dateCalculation.days
         )
-        dateCalculation.days < 1 -> view.resources.getQuantityString(
+        dateCalculation.hours > 1 -> view.resources.getQuantityString(
             R.plurals.hour,
             1,
             dateCalculation.hours
@@ -73,16 +73,16 @@ fun time(view: View, creation_date: Long): String {
             0,
             dateCalculation.hours
         )
-        dateCalculation.hours < 1 -> view.resources.getQuantityString(
+        dateCalculation.minutes > 5 -> view.resources.getQuantityString(
             R.plurals.min,
             1,
             dateCalculation.minutes
         )
-        dateCalculation.minutes <= 5 -> view.resources.getQuantityString(
+        dateCalculation.minutes == 1 -> view.resources.getQuantityString(
             R.plurals.min,
             0,
             dateCalculation.minutes
         )
-        else -> ""
+        else -> view.resources.getString(R.string.seconds, dateCalculation.seconds)
     }
 }
