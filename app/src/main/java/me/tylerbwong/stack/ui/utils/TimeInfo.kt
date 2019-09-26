3a1297b9be391749364df6fa0b172410695cd5d9
Victor Vicari <victor.a.vicari@gmail.com>
2019-09-26 15:54:43 -0300

    minor adjusts

--- app/src/main/java/me/tylerbwong/stack/ui/utils/TimeInfo.kt
+++ app/src/main/java/me/tylerbwong/stack/ui/utils/TimeInfo.kt
@@ -11,77 +11,78 @@ enum class Type {
     EDITED
 }
 
-fun Long.type(view: View, type: Type): String =
+fun type(view: View, type: Type): String =
     when (type) {
         Type.ASKED -> view.resources.getQuantityString(R.plurals.type, 0)
         Type.ANSWERED -> view.resources.getQuantityString(R.plurals.type, 1)
         Type.EDITED -> view.resources.getQuantityString(R.plurals.type, 2)
     }
 
-fun String.time(view: View, creation_date: Long): String {
-    val creationDate = DateTime.fromUnix(creation_date)
-    val dateCalculation = (DateTime.now() until creationDate).span
+fun time(view: View, creation_date: Long): String {
+    val creationDate = DateTime.fromUnix(creation_date * 1000)
+    val dateCalculation = (creationDate until DateTime.now()).span
 
     return when {
-        dateCalculation.minutes <= 5 -> view.resources.getQuantityString(
-            R.plurals.min,
+        dateCalculation.years > 1 -> view.resources.getQuantityString(
+            R.plurals.year,
             0,
-            dateCalculation.minutes
-        )
-        dateCalculation.hours < 1 -> view.resources.getQuantityString(
-            R.plurals.min,
-            1,
-            dateCalculation.minutes
-        )
-        dateCalculation.hours == 1 -> view.resources.getQuantityString(
-            R.plurals.hour,
-            0,
-            dateCalculation.hours
-        )
-        dateCalculation.days < 1 -> view.resources.getQuantityString(
-            R.plurals.hour,
-            1,
-            dateCalculation.hours
-        )
-        dateCalculation.days == 1 -> view.resources.getQuantityString(
-            R.plurals.day,
-            0,
-            dateCalculation.days
-        )
-        dateCalculation.weeks < 1 -> view.resources.getQuantityString(
-            R.plurals.day,
-            1,
-            dateCalculation.days
-        )
-        dateCalculation.weeks == 1 -> view.resources.getQuantityString(
-            R.plurals.week,
-            0,
-            dateCalculation.weeks
-        )
-        dateCalculation.months < 1 -> view.resources.getQuantityString(
-            R.plurals.week,
-            1,
-            dateCalculation.weeks
-        )
-        dateCalculation.months == 1 -> view.resources.getQuantityString(
-            R.plurals.month,
-            0,
-            dateCalculation.months
-        )
-        dateCalculation.years < 1 -> view.resources.getQuantityString(
-            R.plurals.month,
-            1,
-            dateCalculation.months
+            dateCalculation.years
         )
         dateCalculation.years == 1 -> view.resources.getQuantityString(
             R.plurals.year,
             0,
             dateCalculation.years
         )
-        else -> view.resources.getQuantityString(
-            R.plurals.year,
+        dateCalculation.years < 1 -> view.resources.getQuantityString(
+            R.plurals.month,
             1,
-            dateCalculation.years
+            dateCalculation.months
         )
+        dateCalculation.months == 1 -> view.resources.getQuantityString(
+            R.plurals.month,
+            0,
+            dateCalculation.months
+        )
+        dateCalculation.months < 1 -> view.resources.getQuantityString(
+            R.plurals.week,
+            1,
+            dateCalculation.weeks
+        )
+        dateCalculation.weeks == 1 -> view.resources.getQuantityString(
+            R.plurals.week,
+            0,
+            dateCalculation.weeks
+        )
+        dateCalculation.weeks < 1 -> view.resources.getQuantityString(
+            R.plurals.day,
+            1,
+            dateCalculation.days
+        )
+        dateCalculation.days == 1 -> view.resources.getQuantityString(
+            R.plurals.day,
+            0,
+            dateCalculation.days
+        )
+        dateCalculation.days < 1 -> view.resources.getQuantityString(
+            R.plurals.hour,
+            1,
+            dateCalculation.hours
+        )
+        dateCalculation.hours == 1 -> view.resources.getQuantityString(
+            R.plurals.hour,
+            0,
+            dateCalculation.hours
+        )
+        dateCalculation.hours < 1 -> view.resources.getQuantityString(
+            R.plurals.min,
+            1,
+            dateCalculation.minutes
+        )
+        dateCalculation.minutes <= 5 -> view.resources.getQuantityString(
+            R.plurals.min,
+            0,
+            dateCalculation.minutes
+        )
+        else -> ""
     }
 }
