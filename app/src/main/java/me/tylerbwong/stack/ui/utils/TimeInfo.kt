9e9d5a993294723a7851fc287d5df160fe45aa21
Victor <victor.a.vicari@gmail.com>
2019-09-27 08:40:50 -0300

    replaced old type format with TextView extension function

--- app/src/main/java/me/tylerbwong/stack/ui/utils/TimeInfo.kt
+++ app/src/main/java/me/tylerbwong/stack/ui/utils/TimeInfo.kt
@@ -1,6 +1,7 @@
 package me.tylerbwong.stack.ui.utils
 
 import android.view.View
+import android.widget.TextView
 import com.soywiz.klock.DateTime
 import com.soywiz.klock.until
 import me.tylerbwong.stack.R
@@ -18,71 +19,71 @@ fun type(view: View, type: UserActionType): String =
         UserActionType.EDITED -> view.resources.getQuantityString(R.plurals.type, 2)
     }
 
-fun timeCounter(view: View, creation_date: Long): String {
-    val creationDate = DateTime.fromUnix(creation_date * 1000)
+fun TextView.formatTimeForActionType(actionType: UserActionType, timeStamp: Long) {
+    val creationDate = DateTime.fromUnix(timeStamp * 1000)
     val dateCalculation = (creationDate until DateTime.now()).span
 
-    return when {
-        dateCalculation.years > 1 -> view.resources.getQuantityString(
+    when {
+        dateCalculation.years > 1 -> this.resources.getQuantityString(
             R.plurals.year,
             1,
             dateCalculation.years
         )
-        dateCalculation.years == 1 -> view.resources.getQuantityString(
+        dateCalculation.years == 1 -> this.resources.getQuantityString(
             R.plurals.year,
             0,
             dateCalculation.years
         )
-        dateCalculation.months > 1 -> view.resources.getQuantityString(
+        dateCalculation.months > 1 -> this.resources.getQuantityString(
             R.plurals.month,
             1,
             dateCalculation.months
         )
-        dateCalculation.months == 1 -> view.resources.getQuantityString(
+        dateCalculation.months == 1 -> this.resources.getQuantityString(
             R.plurals.month,
             0,
             dateCalculation.months
         )
-        dateCalculation.weeks > 1 -> view.resources.getQuantityString(
+        dateCalculation.weeks > 1 -> this.resources.getQuantityString(
             R.plurals.week,
             1,
             dateCalculation.weeks
         )
-        dateCalculation.weeks == 1 -> view.resources.getQuantityString(
+        dateCalculation.weeks == 1 -> this.resources.getQuantityString(
             R.plurals.week,
             0,
             dateCalculation.weeks
         )
-        dateCalculation.days > 1 -> view.resources.getQuantityString(
+        dateCalculation.days > 1 -> this.resources.getQuantityString(
             R.plurals.day,
             1,
             dateCalculation.days
         )
-        dateCalculation.days == 1 -> view.resources.getQuantityString(
+        dateCalculation.days == 1 -> this.resources.getQuantityString(
             R.plurals.day,
             0,
             dateCalculation.days
         )
-        dateCalculation.hours > 1 -> view.resources.getQuantityString(
+        dateCalculation.hours > 1 -> this.resources.getQuantityString(
             R.plurals.hour,
             1,
             dateCalculation.hours
         )
-        dateCalculation.hours == 1 -> view.resources.getQuantityString(
+        dateCalculation.hours == 1 -> this.resources.getQuantityString(
             R.plurals.hour,
             0,
             dateCalculation.hours
         )
-        dateCalculation.minutes > 5 -> view.resources.getQuantityString(
+        dateCalculation.minutes > 5 -> this.resources.getQuantityString(
             R.plurals.min,
             1,
             dateCalculation.minutes
         )
-        dateCalculation.minutes == 1 -> view.resources.getQuantityString(
+        dateCalculation.minutes == 1 -> this.resources.getQuantityString(
             R.plurals.min,
             0,
             dateCalculation.minutes
         )
-        else -> view.resources.getString(R.string.seconds, dateCalculation.seconds)
+        else -> this.resources.getString(R.string.seconds, dateCalculation.seconds)
     }
 }
