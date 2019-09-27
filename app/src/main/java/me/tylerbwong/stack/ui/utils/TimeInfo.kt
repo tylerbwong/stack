3c0e06f8b43d44eeeb7e1528b4d05349ecf9ff05
Victor <victor.a.vicari@gmail.com>
2019-09-27 13:36:47 -0300

    minor code refactor

--- app/src/main/java/me/tylerbwong/stack/ui/utils/TimeInfo.kt
+++ app/src/main/java/me/tylerbwong/stack/ui/utils/TimeInfo.kt
@@ -1,6 +1,5 @@
 package me.tylerbwong.stack.ui.utils
 
-import android.view.View
 import android.widget.TextView
 import androidx.annotation.StringRes
 import com.soywiz.klock.DateTime
@@ -14,79 +13,80 @@ enum class UserActionType(@StringRes val labelResourceId: Int) {
     EDITED(R.string.edited)
 }
 
-fun type(view: View, type: UserActionType) {
-    when (type) {
-        UserActionType.ASKED -> UserActionType.ASKED.labelResourceId
-        UserActionType.ANSWERED -> UserActionType.ANSWERED.labelResourceId
-        UserActionType.EDITED -> UserActionType.EDITED.labelResourceId
-    }
-}
-
-fun TextView.formatTimeForActionType(actionType: UserActionType, timeStamp: Long) {
+fun TextView.formatTimeForActionType(actionType: UserActionType, timeStamp: Long): String {
     val creationDate = DateTime.fromUnix(timeStamp.milliseconds.millisecondsLong)
     val dateCalculation = (creationDate until DateTime.now()).span
 
-    when {
+    val userAction = when (actionType) {
+        UserActionType.ASKED -> context.getString(UserActionType.ASKED.labelResourceId)
+        UserActionType.ANSWERED -> context.getString(UserActionType.ANSWERED.labelResourceId)
+        UserActionType.EDITED -> context.getString(UserActionType.EDITED.labelResourceId)
+    }
+
+    val timeUserAction = when {
         dateCalculation.years > 1 -> this.resources.getQuantityString(
             R.plurals.year,
-            1,
+            dateCalculation.years,
             dateCalculation.years
         )
         dateCalculation.years == 1 -> this.resources.getQuantityString(
             R.plurals.year,
-            0,
+            dateCalculation.years,
             dateCalculation.years
         )
         dateCalculation.months > 1 -> this.resources.getQuantityString(
             R.plurals.month,
-            1,
+            dateCalculation.months,
             dateCalculation.months
         )
         dateCalculation.months == 1 -> this.resources.getQuantityString(
             R.plurals.month,
-            0,
+            dateCalculation.months,
             dateCalculation.months
         )
         dateCalculation.weeks > 1 -> this.resources.getQuantityString(
             R.plurals.week,
-            1,
+            dateCalculation.weeks,
             dateCalculation.weeks
         )
         dateCalculation.weeks == 1 -> this.resources.getQuantityString(
             R.plurals.week,
-            0,
+            dateCalculation.weeks,
             dateCalculation.weeks
         )
         dateCalculation.days > 1 -> this.resources.getQuantityString(
             R.plurals.day,
-            1,
+            dateCalculation.days,
             dateCalculation.days
         )
         dateCalculation.days == 1 -> this.resources.getQuantityString(
             R.plurals.day,
-            0,
+            dateCalculation.days,
             dateCalculation.days
         )
         dateCalculation.hours > 1 -> this.resources.getQuantityString(
             R.plurals.hour,
-            1,
+            dateCalculation.hours,
             dateCalculation.hours
         )
         dateCalculation.hours == 1 -> this.resources.getQuantityString(
             R.plurals.hour,
-            0,
+            dateCalculation.hours,
             dateCalculation.hours
         )
-        dateCalculation.minutes > 5 -> this.resources.getQuantityString(
+        dateCalculation.minutes > 1 -> this.resources.getQuantityString(
             R.plurals.min,
-            1,
+            dateCalculation.minutes,
             dateCalculation.minutes
         )
         dateCalculation.minutes == 1 -> this.resources.getQuantityString(
             R.plurals.min,
-            0,
+            dateCalculation.minutes,
             dateCalculation.minutes
         )
         else -> this.resources.getString(R.string.seconds, dateCalculation.seconds)
     }
+
+    return "$userAction $timeUserAction"
+
 }
