e308095d2369ac75d85635219aaff3dbfea3efed
Tyler Wong <tbwong3@gmail.com>
2019-12-24 15:45:55 -0800

    Fix draft primary key

--- app/src/main/java/me/tylerbwong/stack/data/persistence/entity/AnswerDraftEntity.kt
+++ app/src/main/java/me/tylerbwong/stack/data/persistence/entity/AnswerDraftEntity.kt
@@ -7,13 +7,10 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "answer_drafts")
 data class AnswerDraftEntity(
     @ColumnInfo(name = "question_id")
+    @PrimaryKey
     val questionId: Int,
     @ColumnInfo(name = "updated_date")
     val updatedDate: Long,
     @ColumnInfo(name = "body_markdown")
     val bodyMarkdown: String
-) {
-    @ColumnInfo(name = "draft_id")
-    @PrimaryKey(autoGenerate = true)
-    var draftId: Long? = null
-}
+)
