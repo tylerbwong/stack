d40d3049c76d76335a34ccac6f13e21ca66e09dc
Tyler Wong <tbwong3@gmail.com>
2020-01-03 23:11:03 -0800

    Fill in search fragment

--- app/src/main/java/me/tylerbwong/stack/ui/search/SearchFragment.kt
+++ app/src/main/java/me/tylerbwong/stack/ui/search/SearchFragment.kt
@@ -1,5 +1,32 @@
 package me.tylerbwong.stack.ui.search
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_home.*
+import me.tylerbwong.stack.R
+import me.tylerbwong.stack.ui.home.HeaderItem
+import me.tylerbwong.stack.ui.home.HomeAdapter
+import me.tylerbwong.stack.ui.home.HomeItem
 
-internal class SearchFragment : Fragment()
+class SearchFragment : Fragment(R.layout.fragment_home) {
+
+    private val adapter = HomeAdapter()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView.apply {
+            adapter = this@SearchFragment.adapter
+            layoutManager = LinearLayoutManager(context)
+        }
+
+        val homeItems: List<HomeItem> = listOf(
+            HeaderItem(
+                getString(R.string.search),
+                "Coming soon!"
+            )
+        )
+
+        adapter.submitList(homeItems)
+    }
+}
