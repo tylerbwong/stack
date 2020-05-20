61fbe40190e5712f35b7dea6d360372be0cd7945
Tyler Wong <tbwong3@gmail.com>
2020-05-19 23:52:09 -0700

    Remove default search payload values for testing

--- app/src/main/java/me/tylerbwong/stack/data/model/SearchPayload.kt
+++ app/src/main/java/me/tylerbwong/stack/data/model/SearchPayload.kt
@@ -4,11 +4,11 @@ sealed class SearchPayload {
 
     data class Standard(
         val query: String,
-        val isAccepted: Boolean? = false,
-        val minNumAnswers: Int? = 4,
-        val bodyContains: String? = "edittext",
-        val isClosed: Boolean? = false,
-        val tags: List<String>? = listOf("android"),
+        val isAccepted: Boolean? = null,
+        val minNumAnswers: Int? = null,
+        val bodyContains: String? = null,
+        val isClosed: Boolean? = null,
+        val tags: List<String>? = null,
         val titleContains: String? = null,
         val searchId: Int? = null
     ) : SearchPayload()
