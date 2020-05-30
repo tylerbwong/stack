041090a4c5f30c39159c4c5e74b32b8e2b904316
Tyler Wong <tbwong3@gmail.com>
2020-05-29 23:34:09 -0700

    Fix

--- app/src/main/java/me/tylerbwong/stack/ui/search/filters/FiltersLayout.kt
+++ app/src/main/java/me/tylerbwong/stack/ui/search/filters/FiltersLayout.kt
@@ -1,4 +1,4 @@
-@file:Suppress("FunctionNaming", "MagicNumber", )
+@file:Suppress("FunctionNaming", "MagicNumber")
 package me.tylerbwong.stack.ui.search.filters
 
 import androidx.compose.Composable
