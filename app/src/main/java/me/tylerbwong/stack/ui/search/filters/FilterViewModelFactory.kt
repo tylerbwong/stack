a6bf87f728dfea57a9656b6aee541b941e93168c
Tyler Wong <tbwong3@gmail.com>
2020-06-13 01:58:07 -0700

    Fix lint

--- app/src/main/java/me/tylerbwong/stack/ui/search/filters/FilterViewModelFactory.kt
+++ app/src/main/java/me/tylerbwong/stack/ui/search/filters/FilterViewModelFactory.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import javax.inject.Inject
 
-class FilterViewModelFactory @Inject constructor(): ViewModelProvider.Factory {
+class FilterViewModelFactory @Inject constructor() : ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
