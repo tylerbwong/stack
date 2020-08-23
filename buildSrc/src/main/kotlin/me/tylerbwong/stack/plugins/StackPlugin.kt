bcfa55f760a6ba9c426afc4f022f2b6b364c8fd2
Tyler Wong <tbwong3@gmail.com>
2020-08-23 14:32:18 -0700

    Add source set

--- buildSrc/src/main/kotlin/me/tylerbwong/stack/plugins/StackPlugin.kt
+++ buildSrc/src/main/kotlin/me/tylerbwong/stack/plugins/StackPlugin.kt
@@ -10,6 +10,7 @@ import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.get
 import org.gradle.kotlin.dsl.getByType
 import org.jlleitschuh.gradle.ktlint.KtlintExtension
 import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
@@ -71,6 +72,8 @@ class StackPlugin : Plugin<Project> {
         testOptions {
             unitTests.isIncludeAndroidResources = true
         }
+
+        sourceSets["main"].java.srcDir("src/main/kotlin")
     }
 
     private fun configureStaticAnalysis(project: Project) {
