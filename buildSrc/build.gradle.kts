c8092b222bc3853fbaeb345fcbeba62e486f35a1
Tyler Wong <tbwong3@gmail.com>
2020-08-23 12:32:53 -0700

    Update buildSrc detekt version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,5 +12,5 @@ dependencies {
     implementation("com.android.tools.build:gradle:4.2.0-alpha07")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.0")
     implementation("org.jlleitschuh.gradle:ktlint-gradle:9.2.1")
-    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.11.1")
+    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.11.2")
 }
