44cf5674421ea50ad183f49e96d55270684ffd56
Tyler Wong <tbwong3@gmail.com>
2020-10-02 00:54:02 -0700

    Update Drafts.kt

--- app/src/main/kotlin/me/tylerbwong/stack/ui/drafts/Drafts.kt
+++ app/src/main/kotlin/me/tylerbwong/stack/ui/drafts/Drafts.kt
@@ -14,8 +14,6 @@ import androidx.compose.material.ripple.RippleIndication
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ContextAmbient
