0815d278f8c1d75062bbf2924e324585f4853d63
Tyler Wong <tbwong3@gmail.com>
2020-10-13 21:57:38 -0700

    Prepare Release 1.0.0-alpha02

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -3,6 +3,6 @@ object AndroidConfig {
     const val COMPILE_SDK = 30
     const val MIN_SDK = 21
     const val TARGET_SDK = 30
-    const val VERSION_CODE = 7
-    const val VERSION_NAME = "1.0.0-alpha01"
+    const val VERSION_CODE = 8
+    const val VERSION_NAME = "1.0.0-alpha02"
 }
