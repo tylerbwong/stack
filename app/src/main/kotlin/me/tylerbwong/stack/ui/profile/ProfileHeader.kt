ceddf61ee9f4f2a3294c2390efe214a5246af49b
Tyler Wong <tbwong3@gmail.com>
2020-10-19 23:36:13 -0700

    Fix padding for long locations on profiles

--- app/src/main/kotlin/me/tylerbwong/stack/ui/profile/ProfileHeader.kt
+++ app/src/main/kotlin/me/tylerbwong/stack/ui/profile/ProfileHeader.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
@@ -35,7 +36,12 @@ import me.tylerbwong.stack.ui.utils.toHtml
 @Composable
 fun ProfileHeader(user: User) {
     Column(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                start = 16.dp,
+                end = 16.dp
+            ),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         val primaryTextColor = colorResource(R.color.primaryTextColor)
