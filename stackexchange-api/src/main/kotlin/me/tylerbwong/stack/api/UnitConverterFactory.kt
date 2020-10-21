4d8be97f78b64401ec622d671adbe110c6a13087
Tyler Wong <tbwong3@gmail.com>
2020-10-20 19:57:39 -0700

    Make UnitConverterFactory internal

--- stackexchange-api/src/main/kotlin/me/tylerbwong/stack/api/UnitConverterFactory.kt
+++ stackexchange-api/src/main/kotlin/me/tylerbwong/stack/api/UnitConverterFactory.kt
@@ -6,7 +6,7 @@ import retrofit2.Converter
 import retrofit2.Retrofit
 import java.lang.reflect.Type
 
-object UnitConverterFactory : Converter.Factory() {
+internal object UnitConverterFactory : Converter.Factory() {
     override fun responseBodyConverter(
         type: Type,
         annotations: Array<out Annotation>,
