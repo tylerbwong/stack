ed190fb2540e86a085bafa996c379c919da40537
Tyler Wong <tbwong3@gmail.com>
2020-10-20 17:49:59 -0700

    Update AGP and Detekt

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,8 +9,8 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:4.2.0-alpha13")
+    implementation("com.android.tools.build:gradle:4.2.0-alpha14")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
     implementation("org.jlleitschuh.gradle:ktlint-gradle:9.3.0")
-    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.14.1")
+    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.14.2")
 }
