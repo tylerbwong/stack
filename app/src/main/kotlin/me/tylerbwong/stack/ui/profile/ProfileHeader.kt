8bf686c5ad117cb8bb276cf77b41ad0dd339beb4
Tyler Wong <tbwong3@gmail.com>
2022-09-05 03:00:22 -0700

    Text styles and profile header clickable

--- app/src/main/kotlin/me/tylerbwong/stack/ui/profile/ProfileHeader.kt
+++ app/src/main/kotlin/me/tylerbwong/stack/ui/profile/ProfileHeader.kt
@@ -3,6 +3,7 @@
 package me.tylerbwong.stack.ui.profile
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -16,10 +17,10 @@ import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -49,8 +50,9 @@ fun ProfileHeader(user: User) {
             val context = LocalContext.current
             val userProfileClickableModifier = user.link?.let {
                 Modifier.clickable(
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = null,
                     onClick = { context.launchUrl(it) },
-                    role = Role.Image,
                 )
             } ?: Modifier
             AsyncImage(
