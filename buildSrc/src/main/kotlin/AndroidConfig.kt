4818fb99a09ab0b20620a48302d7c423bdc82b08
Tyler Wong <tbwong3@gmail.com>
2023-02-20 20:59:39 -0800

    Update version code

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -3,6 +3,6 @@ object AndroidConfig {
     const val COMPILE_SDK = 33
     const val MIN_SDK = 26
     const val TARGET_SDK = 33
-    const val VERSION_CODE = 14
+    const val VERSION_CODE = 15
     const val VERSION_NAME = "2.0.0"
 }
