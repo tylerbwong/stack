24aed2b52ac1955564813b22ddec0c552efabeda
Tyler Wong <tbwong3@gmail.com>
2023-02-22 21:04:25 -0800

    Use WindowInsetsControllerCompat and bump appcompat

--- app/src/main/kotlin/me/tylerbwong/stack/ui/theme/ThemeManager.kt
+++ app/src/main/kotlin/me/tylerbwong/stack/ui/theme/ThemeManager.kt
@@ -4,9 +4,9 @@ import android.app.Activity
 import android.app.Application
 import android.content.Context
 import android.content.res.Configuration
-import android.view.View
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.preference.PreferenceManager
 import com.google.android.material.color.DynamicColors
 
@@ -33,45 +33,13 @@ object ThemeManager {
     }
 
     fun injectTheme(activity: Activity) {
-        if (activity.isNightModeEnabled) {
-            removeLightStatusBarIfSupported(activity)
-            removeLightNavigationBarIfSupported(activity)
-        } else {
-            setLightStatusBarIfSupported(activity)
-            setLightNavigationBarIfSupported(activity)
+        val isNightModeEnabled = activity.isNightModeEnabled
+        val window = activity.window
+        val decorView = window.decorView
+        with(WindowInsetsControllerCompat(window, decorView)) {
+            isAppearanceLightStatusBars = !isNightModeEnabled
+            isAppearanceLightNavigationBars = !isNightModeEnabled
         }
-    }
-
-    @Suppress("deprecation") // TODO Update to Android R APIs
-    private fun setLightStatusBarIfSupported(activity: Activity) {
-        val decorView = activity.window.decorView
-        var flags = decorView.systemUiVisibility
-        flags = flags or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-        decorView.systemUiVisibility = flags
-    }
-
-    @Suppress("deprecation") // TODO Update to Android R APIs
-    private fun removeLightStatusBarIfSupported(activity: Activity) {
-        val decorView = activity.window.decorView
-        var flags = decorView.systemUiVisibility
-        flags = flags and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
-        decorView.systemUiVisibility = flags
-    }
-
-    @Suppress("deprecation") // TODO Update to Android R APIs
-    private fun setLightNavigationBarIfSupported(activity: Activity) {
-        val decorView = activity.window.decorView
-        var flags = decorView.systemUiVisibility
-        flags = flags or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
-        decorView.systemUiVisibility = flags
-    }
-
-    @Suppress("deprecation") // TODO Update to Android R APIs
-    private fun removeLightNavigationBarIfSupported(activity: Activity) {
-        val decorView = activity.window.decorView
-        var flags = decorView.systemUiVisibility
-        flags = flags and View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR.inv()
-        decorView.systemUiVisibility = flags
     }
 
     fun toggleTheme(context: Context, newMode: Int) {
